4e3213d21af8416e75373f1b0a31f6800b6d56d5
Satanisto <Khmilevoi@gmail.com>
2017-08-01 20:32:42 +0300

    port

---
+++ portfolio/js/common.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+	// // Плавный скролл по ссылке
+	//
+	// $(".local-link").on('click', 'a', function() {
+	// 	event.preventDefault();
+	// 	var id 	= $(this).attr('href'),
+	// 	top = $(id).offset().top
+	// 	$("html,body").animate({scrollTop: top}, 500);
+	// });
+
+	// Меню (mobile)
+
+	$(".button-menu").click(function () {
+		$("body").toggleClass("active-button-menu");
+	})
+
+	// Ссылки меню
+
+	$(".main-menu--list-item-1").click(function () {
+		$("body").addClass("active-link-1"),
+		$("body").removeClass("active-link-2"),
+		$("body").removeClass("active-link-3"),
+		$("body").removeClass("active-link-4"),
+		$("body").removeClass("active-button-menu");
+	});
+
+	$(".main-menu--list-item-2").click(function () {
+		$("body").addClass("active-link-2"),
+		$("body").removeClass("active-link-1"),
+		$("body").removeClass("active-link-3"),
+		$("body").removeClass("active-link-4"),
+		$("body").removeClass("active-button-menu");
+	});
+
+	$(".main-menu--list-item-3").click(function () {
+		$("body").addClass("active-link-3"),
+		$("body").removeClass("active-link-1"),
+		$("body").removeClass("active-link-2"),
+		$("body").removeClass("active-link-4"),
+		$("body").removeClass("active-button-menu");
+	});
+
+	$(".main-menu--list-item-4").click(function () {
+		$("body").addClass("active-link-4"),
+		$("body").removeClass("active-link-1"),
+		$("body").removeClass("active-link-2"),
+		$("body").removeClass("active-link-3"),
+		$("body").removeClass("active-button-menu");
+	});
+
+});
