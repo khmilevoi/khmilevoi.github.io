3005af0129f80144096b831e23becef85873a3a9
Satanisto <Khmilevoi@gmail.com>
2018-10-20 22:31:57 +0300

    sort

--- js/sort/common.js
+++ js/sort/common.js
@@ -1,34 +1,17 @@
 canvasWidth = 1000;
 canvasHeight = 500;
-speed = 1000;
-n = 1000;
+speed = 50000000;
+n = 100;
 x = 0;
 y = 0;
 z = 0;
 let mas = [];
-for (let i = 0; i < n; i++) {
-    pos = Math.floor(Math.random() * (n - 1) + 1);
-    r = Math.floor(Math.random() * (256));
-    g = Math.floor(Math.random() * (256));
-    b = Math.floor(Math.random() * (256));
-    gr = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-    // gr = 0.299 * r + 0.587 * g + 0.114 * b;
-    sz = canvasHeight;
-    mas.push({
-        pos: pos,
-        sz: sz,
-        r: r,
-        g: g,
-        b: b,
-        gr: gr,
-        color: '#' + r.toString(16) + g.toString(16) + b.toString(16)
-    });
-}
+
 makeBody = () => {
     document.body.innerHTML =
         "\
 	<div id = 'wrapper'>\
-    	<canvas id = 'game'></canvas>\
+    	<canvas id = 'draw'></canvas>\
 	</div>\
 	";
     document.body.style.margin = "0px";
@@ -42,21 +25,45 @@ makeBody = () => {
     wrapper.style.alignItems = "center";
 };
 
+set = () => {
+    for (let i = 0; i < n; i++) {
+        pos = Math.floor(Math.random() * (n - 1) + 1);
+        r = Math.floor(Math.random() * (256));
+        g = Math.floor(Math.random() * (256));
+        b = Math.floor(Math.random() * (256));
+        gr = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+        // gr = 0.3 * r + 0.3 * g + 0.3 * b;
+        // gr = 0.299 * r + 0.587 * g + 0.114 * b;
+        sz = canvasHeight;
+        mas.push({
+            pos: pos,
+            sz: (gr / 255) * sz,
+            r: r,
+            g: g,
+            b: b,
+            gr: gr,
+            color: '#' + r.toString(16) + g.toString(16) + b.toString(16)
+        });
+    }
+}
+
 makeCanvas = () => {
-    canvas = document.getElementById("game");
+    canvas = document.getElementById("draw");
     canvas.width = canvasWidth;
     canvas.height = canvasHeight;
     context = canvas.getContext("2d");
 };
 
 window.onload = () => {
+    n = prompt("Amount: ", 100);
     makeBody();
     makeCanvas();
-    setInterval(game, 1000 / speed);
+    set();
+    setInterval(draw, 1000 / speed);
 };
 
-game = () => {
-    context.fillStyle = "black";
+draw = () => {
+    context.fillStyle = "lavender";
     context.fillRect(0, 0, canvasWidth, canvasHeight);
 
     art(n);
