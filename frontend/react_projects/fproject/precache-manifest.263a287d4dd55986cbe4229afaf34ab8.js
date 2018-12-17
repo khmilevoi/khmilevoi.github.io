85af937645956396963de0609bcee301e7181e3a
Satanisto <Khmilevoi@gmail.com>
2018-12-17 03:06:34 +0200

    deploy

---
+++ frontend/react_projects/fproject/precache-manifest.263a287d4dd55986cbe4229afaf34ab8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "960a9c8ad987ec378c84511f3ff909fb",
+    "url": "/frontend/react_project/fproject/static/media/slide-4.960a9c8a.png"
+  },
+  {
+    "revision": "310bfd6edb498a4a123e9c1d75147465",
+    "url": "/frontend/react_project/fproject/static/media/slide-3.310bfd6e.png"
+  },
+  {
+    "revision": "6b3e18979fe0068d37051f78d393df4f",
+    "url": "/frontend/react_project/fproject/static/media/slide-2.6b3e1897.png"
+  },
+  {
+    "revision": "f542f665676159780489b03f159a558c",
+    "url": "/frontend/react_project/fproject/static/media/slide-1.f542f665.png"
+  },
+  {
+    "revision": "d3a2198e6d117ccce2e1",
+    "url": "/frontend/react_project/fproject/static/js/runtime~main.d3a2198e.js"
+  },
+  {
+    "revision": "17de91acd1ae9d158175",
+    "url": "/frontend/react_project/fproject/static/js/main.17de91ac.chunk.js"
+  },
+  {
+    "revision": "e95a94d4cac2e7a99c97",
+    "url": "/frontend/react_project/fproject/static/js/1.e95a94d4.chunk.js"
+  },
+  {
+    "revision": "17de91acd1ae9d158175",
+    "url": "/frontend/react_project/fproject/static/css/main.cbbb45ef.chunk.css"
+  },
+  {
+    "revision": "88db59f734a17bbc9b5bda90c4ce41d5",
+    "url": "/frontend/react_project/fproject/index.html"
+  }
+];
